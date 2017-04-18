e37aaf0edee62d6a699e1cccef11bc79751efbaa
Sam Robinson <samuel.vj.robinson@gmail.com>
2017-04-18 16:18:40 -0600

    Changed patch-level competition argument

--- R/curr.R
+++ R/curr.R
@@ -1,7 +1,7 @@
 #Function to calculate currency(-ies) in a given cell. Called by optimLoadCurr
-curr=function(L_i,L_max_i,n_i,h_i,p_i,f_i,d_i,v_i,beta_i,H_i,c_i,c_f,whatCurr_i,mu,l,e,NumFls,sumAll=T,Spatch=F){
+curr=function(L_i,L_max_i,n_i,h_i,p_i,f_i,d_i,v_i,beta_i,H_i,c_i,c_f,whatCurr_i,mu,l,e,NumFls,sumAll=T,patchLev=F){
   Stemp=scalFun(mu=mu, l=l, L_i=L_i, L_max=L_max_i, n_i=n_i, #Scaling function for cell
-                h_i=h_i, p_i=p_i, f_i=f_i, d_i=d_i, v_i=v_i, beta_i=beta_i, H_i=H_i,NumFls=NumFls,patchLev=Spatch)
+                h_i=h_i, p_i=p_i, f_i=f_i, d_i=d_i, v_i=v_i, beta_i=beta_i, H_i=H_i,NumFls=NumFls,patchLev=patchLev)
   #Vector of currencies for cell, using getCurr to return appropriate nest-specific currency
   currs=mapply(getCurr,whatCurr=whatCurr_i, L=L_i,L_max=L_max_i, e=e, d=d_i, v=v_i, h=h_i,
                f=f_i, l=l, p_i=p_i, c_i=c_i, c_f=c_f, H=H_i, beta=beta_i,Stemp)
