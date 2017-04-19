8f6d6984ea80ce3d97e599f1ac0f10db064c9ba2
Sam Robinson <samuel.vj.robinson@gmail.com>
2017-04-19 13:21:24 -0600

    Reworked to include world structure (as well as nests), and simplified re-naming procedure in dyplr

--- R/nests2df.R
+++ R/nests2df.R
@@ -1,20 +1,27 @@
 #Convert model runs to dataframes
-nests2df=function(nests){
+nests2df=function(modelRun){
   require(dplyr)
   require(tidyr)
   convertNest=function(i,nes){
     nNests=bind_rows(data.frame(nes[[i]]$n,rows=0:(nrow(nes[[i]]$n)-1),category='numForagers'), #Occupancy
-                     data.frame(nes[[i]]$L,rows=0:(nrow(nes[[i]]$n)-1),category='loadSize'), #Load size
-                     data.frame(nes[[i]]$curr,rows=0:(nrow(nes[[i]]$n)-1),category='currency'), #Currency
-                     data.frame(nes[[i]]$d,rows=0:(nrow(nes[[i]]$n)-1),category='distance'), #Distance to patch
-                     data.frame(nes[[i]]$loadingTime,rows=0:(nrow(nes[[i]]$n)-1),category='loadingTime'), #Time spent loading
-                     data.frame(nes[[i]]$travelTime,rows=0:(nrow(nes[[i]]$n)-1),category='travelTime'), #Travel time to patch
-                     data.frame(nes[[i]]$boutLength,rows=0:(nrow(nes[[i]]$n)-1),category='boutLength')) #Total length of foraging bout
-    names(nNests)[1:nrow(nes[[i]]$n)]=c(0:(nrow(nes[[i]]$n)-1))
-    nNests=gather(nNests,cols,value,-rows:-category,convert=T) %>%
-      spread(category,value) %>%
-      mutate(nest=i,rate=nes[[i]]$rate,sol=nes[[i]]$sol)
+               data.frame(nes[[i]]$L,rows=0:(nrow(nes[[i]]$n)-1),category='loadSize'), #Load size
+               data.frame(nes[[i]]$curr,rows=0:(nrow(nes[[i]]$n)-1),category='currency'), #Currency
+               data.frame(nes[[i]]$d,rows=0:(nrow(nes[[i]]$n)-1),category='distance'), #Distance to patch
+               data.frame(nes[[i]]$loadingTime,rows=0:(nrow(nes[[i]]$n)-1),category='loadingTime'), #Time spent loading
+               data.frame(nes[[i]]$travelTime,rows=0:(nrow(nes[[i]]$n)-1),category='travelTime'), #Travel time to patch
+               data.frame(nes[[i]]$boutLength,rows=0:(nrow(nes[[i]]$n)-1),category='boutLength')) %>% #Length of foraging bout
+    setNames(gsub('X','',names(.))) %>% #Remove "X" from names
+    gather(cols,value,-rows:-category,convert=T) %>% #Gather into single column
+    spread(category,value) %>% #Spread over categories
+    mutate(nest=i,whichCurr=nes[[i]]$whatCurr,sol=nes[[i]]$sol) #Add nest number, currency, and solitary foraging
     return(nNests)
   }
-  return(bind_rows(lapply(1:length(nests),nes=nests,convertNest)))
+
+  #Warnings about factor to character conversion suppressed
+  nests=suppressWarnings(bind_rows(lapply(1:length(modelRun$nests),nes=modelRun$nests,convertNest)))
+  world=data.frame(modelRun$world$S,rows=0:(nrow(modelRun$world$S)-1),category='S') %>%
+    setNames(gsub('X','',names(.))) %>%
+    gather(cols,value,-rows:-category) %>%
+    spread(category,value)
+  return(list(nests=nests,world=world))
 }
