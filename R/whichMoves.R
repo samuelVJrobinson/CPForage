4b0037d64b2d714c45d297d88746445d41bd81fe
Sam Robinson <samuel.vj.robinson@gmail.com>
2017-05-03 17:52:55 -0600

    Added function to figure out which cells foragers should be moved FROM and TO

---
+++ R/whichMoves.R
@@ -0,0 +1,16 @@
+whichMoves=function(nests=NA,world=NA,i=NA,soc=NA){
+  #Function to find return list with best and worst cells
+  if(is.na(soc)){
+    stop('Sociality not specified')
+  } else if(is.na(nests)){
+    stop('Nests not specified')
+  } else if(is.na(world)){
+    stop('World not specified')
+  } else if(is.na(i)){
+    stop('Which nest not specified')
+  } else if(is.na(soc)){
+
+  }
+
+
+}
